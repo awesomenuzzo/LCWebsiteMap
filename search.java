3c2ae828db5824632da1aa075c34a58c41f85f80
etanabe17 <emilytanabe@lclark.edu>
2024-03-05 14:08:39 -0800

    csv reading search

--- search.java
+++ search.java
@@ -1,16 +1,40 @@
 package fowlMap;
 import edu.princeton.cs.algs4.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 
 public class search {
 
-    public static TrieSET populateTrie(String[] poi){
+    public static ArrayList<String> fileIn(String fileName) {
+        ArrayList<String> poi = new ArrayList<String>();
+        String line = "";
+        String splitBy = ",";
+
+        try {
+            //parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] row = line.split(splitBy);    // use comma as separator
+                poi.add(row[1]);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return poi;
+    }
+
+
+    public static TrieSET populateTrie(ArrayList<String> poi){
         TrieSET data = new TrieSET();
         for (String s: poi) {
+            s = s.toLowerCase();
             data.add(s);
 //            System.out.println(s);
         }
@@ -43,15 +67,10 @@ public class search {
 
     //test with pre-existing string
     public static void main(String[] args){
-        String text =  "We the People of the United States, in Order to form a more perfect " +
-                "Union, establish Justice, insure domestic Tranquility, provide for the " +
-                "common defence, promote the general Welfare, and secure the Blessings " +
-                "of Liberty to ourselves and our Posterity, do ordain and establish " +
-                "this Constitution for the United States of America.";
-        String[] input = text.split(" ");
-
+//
+        ArrayList<String> input = fileIn("src/fowlMap/fowlerNames4.csv");
         TrieSET tree = populateTrie(input);
-        ArrayList<String> result = search("t", tree);
+        ArrayList<String> result = search("bathroom", tree);
         for (String s: result) {
             System.out.print(s + "\n");
         }
