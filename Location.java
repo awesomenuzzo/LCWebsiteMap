671e0125beba5788b8c63e94dfcd9b9c1efcb9fb
etanabe17 <emilytanabe@lclark.edu>
2024-04-02 10:04:19 -0700

    Location update

--- Location.java
+++ Location.java
@@ -13,34 +13,34 @@ import java.io.IOException;
 public class Location {
     int nodeID;
 
-    public static ArrayList<Room> readRoomsFromCSV(String fileName) {
-        ArrayList<Room> rooms = new ArrayList<>();
-        Path pathToFile = Paths.get(fileName);
-//        String line = "";
-        String splitBy = ",";
-        try {
-            //parsing a CSV file into BufferedReader class constructor
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            String line = br.readLine();
-            while ((line = br.readLine()) != null)   //returns a Boolean value
-            {
-                String[] row = line.split(splitBy); // use comma as separator
-                Room l = createlocation(row);
-                rooms.add(l);
-//                line = br.readline();
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        return rooms;
-    }
+//    public static ArrayList<Room> readRoomsFromCSV(String fileName) {
+//        ArrayList<Room> rooms = new ArrayList<>();
+//        Path pathToFile = Paths.get(fileName);
+////        String line = "";
+//        String splitBy = ",";
+//        try {
+//            //parsing a CSV file into BufferedReader class constructor
+//            BufferedReader br = new BufferedReader(new FileReader(fileName));
+//            String line = br.readLine();
+//            while ((line = br.readLine()) != null)   //returns a Boolean value
+//            {
+//                String[] row = line.split(splitBy); // use comma as separator
+//                Room l = createlocation(row);
+//                rooms.add(l);
+////                line = br.readline();
+//            }
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        return rooms;
+//    }
 
-    private static Room createlocation(String[] dataSet) {
-        String name = dataSet[1];
-        String roomNumber = dataSet[0];
-        int floorNumber = Integer.parseInt(dataSet[2]);
-        return new Room(name, roomNumber, floorNumber);
-    }
+//    private static Room createlocation(String[] dataSet) {
+//        String name = dataSet[1];
+//        String roomNumber = dataSet[0];
+//        int floorNumber = Integer.parseInt(dataSet[2]);
+//        return new Room(name, roomNumber, floorNumber, nodeID);
+//    }
 
     public static class Room extends Location {
         private String name;
@@ -48,10 +48,12 @@ public class Location {
         private int floorNumber ;
         private int nodeID;
 
-        public Room(String name, String roomNumber, int floorNumber){
+        public Room(String name, String roomNumber, int floorNumber, int nodeID){
             this.name = name;
             this.roomNumber = roomNumber;
-            this.floorNumber = floorNumber; }
+            this.floorNumber = floorNumber;
+            this.nodeID = nodeID;
+        }
 
         public String getName() { return name; }
         public void setName(String name) { this.name = name; }
@@ -67,12 +69,12 @@ public class Location {
 
 
 
-    public static void main(String[] args){
+//    public static void main(String[] args){
+////
+//        ArrayList<Room> rooms = readRoomsFromCSV("src/fowlMap/fowlerNames4.csv");
 //
-        ArrayList<Room> rooms = readRoomsFromCSV("src/fowlMap/fowlerNames4.csv");
-
-
-    }
+//
+//    }
 
     //test with pre-existing string
 
